ed063d278970cbb3f8736684c3570f582edf18fb
Jamshid <mamatovjamshid506@gmail.com>
2021-01-05 23:33:53 +0500

    I separated the elements in the forks

--- shopping_cart_str.py
+++ shopping_cart_str.py
@@ -2,6 +2,16 @@ from pprint import pprint
 import json
 
 f=open("shopping_cart.json",'r',encoding='utf8',errors='ignore')
-
-       
-    
+data=f.read().split(',')
+# print(data)
+product=[]
+for item in data:
+    ind=item.index(':')
+    product.append(item[ind+1:])
+for i, j in enumerate(product):
+    product[i] = j.replace(' \n}','')
+for i, j in enumerate(product):
+    product[i] = j.replace('"','')   
+for i, j in enumerate(product):
+    product[i] = j.replace('$','') 
+print(product)
