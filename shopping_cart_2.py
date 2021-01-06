3a96e5601711384b6f6bb4ec5a094bdde0ada1ff
Jamshid <mamatovjamshid506@gmail.com>
2021-01-06 22:57:44 +0500

    The second element of the list in data.json has defined some method index separating price and quantity and adding it to sum

--- shopping_cart_2.py
+++ shopping_cart_2.py
@@ -13,3 +13,17 @@ price=float(data[ind2+3:ind2_-1])
 quantity=float(data[ind3+1:ind3_])
 
 sum_=price*quantity
+
+ind = data.index(':',ind3+1)
+ind_=data.index(",",ind3_+2)
+ind2=data.index(":",ind+1)
+ind2_=data.index(",",ind_+1)
+ind3=data.index(":",ind2+1)
+ind3_=data.index("}",ind3+1)
+
+
+price=float(data[ind2+3:ind2_-1])
+quantity=float(data[ind3+1:ind3_])
+
+
+sum_+=price*quantity
