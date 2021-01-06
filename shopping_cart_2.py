e54adb42e1c2928829718a1af7763c752106b6d3
Jamshid <mamatovjamshid506@gmail.com>
2021-01-06 22:30:50 +0500

    The first element of the list in data.json has defined some method indexes

--- shopping_cart_2.py
+++ shopping_cart_2.py
@@ -1,3 +1,10 @@
 import json
 f= open("data_json.json",'r',encoding='utf8',errors='ignore')
 data=f.read()
+
+ind = data.index(':')
+ind_=data.index(",")
+ind2=data.index(":",ind+1)
+ind2_=data.index(",",ind_+1)
+ind3=data.index(":",ind2+1)
+ind3_=data.index("}",ind3+1)
